cfe26408153aee320ce87294172b2396cd79e10a
ilyaszuh <144069160+ilyaszuh@users.noreply.github.com>
2024-04-11 12:21:15 -0700

    Update String_substring_1.s

--- String_substring_1.s
+++ String_substring_1.s
@@ -24,26 +24,26 @@ String_substring_1:
     mov x19, x0         //x19 = x0
     mov x20, x1         //X20 = x1
     mov x21, x2         //X21 = x2
-    mov x22, x30        //Preserve lr
-    bl String_length    //Branch and link to String_length
-    mov x30, x22        //Re-point lr to next addr
 
     sub x0, x21, x20    //Find delta length
-    add x0, x0, #1      //Add 1 to include the null     
+    //add x0, x0, #1      //Add 1 to include the null   
+    str x0,[sp, #-16]!  
     mov x22, x30        //Preserve lr
     bl malloc           //Branch and link to malloc
     mov x30, x22        //Re-point lr to next addr
     
-    add x21, x21, #1    //Add 1 to the total length of the string
+    ldr x2,[sp], #16
+    mov x3, 0x0
 
     string_substring_1_loop:
-        cmp x20, x21                        //Compare the index to the length
+        cmp x3, x2                        //Compare the index to the length
         b.eq string_substring_1_return      //Branch if equal
 
         ldrb w1,[x19, x20]                  //Load a byte from x19 into w1
-        strb w1,[x0, x20]                   //Store the byte in x0
+        strb w1,[x0, x3]                   //Store the byte in x0
         
         add x20, x20, #1                    //Incriment the counter
+        add x3, x3, #1
 
         b string_substring_1_loop           //Branch to the top of the loop                
 
