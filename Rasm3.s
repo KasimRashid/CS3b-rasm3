f5336b8e15090b3da0d4589cad5b1e668dd0ad38
ilyaszuh <144069160+ilyaszuh@users.noreply.github.com>
2024-04-11 14:24:47 -0700

    Update Rasm3.s

--- Rasm3.s
+++ Rasm3.s
@@ -11,56 +11,56 @@
     szPrompt:       .asciz "Enter a string: "       //Prompt 
     szFalse:        .asciz "FALSE"					//False
     szTrue:         .asciz "TRUE"					//True
-    szS1eq:         .asciz "s1 = "
-    szS2eq:         .asciz "s2 = "
-    szS3eq:         .asciz "s3 = "
-    szS4eq:         .asciz "s4 = "
+    szS1eq:         .asciz "s1 = "					//String 1 output
+    szS2eq:         .asciz "s2 = "					//String 1 output
+    szS3eq:         .asciz "s3 = "					//String 1 output
+    szS4eq:         .asciz "s4 = "					//String 1 output
 
     szS1:           .skip BUFFER                    //String 1
     szS2:           .skip BUFFER                    //String 2
     szS3:           .skip BUFFER                    //String 3
 
-    szrp:           .asciz "Cot in the hot." 
-    szhat:          .asciz "hat."
-    szCat:          .asciz "Cat"
-    szinHat:        .asciz "in the hat."
-    szegg:          .asciz "egg"
-    szSpace:        .asciz " "
+    szrp:           .asciz "Cot in the hot." 		//Copy of sz1 after replace
+    szhat:          .asciz "hat."					//hat
+    szCat:          .asciz "Cat"					//cat
+    szinHat:        .asciz "in the hat."			//in the hat
+    szegg:          .asciz "egg"					//egg
+    szSpace:        .asciz " "						// 
 
     szBuffer:       .skip BUFFER                    //Buffer
     dbPtr:          .quad 0                         //Pointer
     dbPtr2:         .quad 0                         //Pointer
 
-    szLen1:         .asciz "s1.length() = " //Length of str1
-    szLen2:         .asciz "s2.length() = " //Length of str2
-    szLen3:         .asciz "s3.length() = " //Length of str3
-    szEquals:       .asciz "String_equals(s1,s3) = "
-    szEquals2:      .asciz "String_equals(s1,s1) = "
-    szEqIgnore:     .asciz "String_equalsIgnoreCase(s1,s3) = "
-    szEqIgnore2:    .asciz "String_equalsIgnoreCase(s1,s2) = "
-    szCpy:          .asciz "s4 = String_copy(s1)"
-    szSubStr1:      .asciz "String_substring_1(s3,4,14) = "
-    szSubStr2:      .asciz "String_substring_2(s3,7) = "
-    szCharAt:       .asciz "String_charAt(s2,4) = "
-    szStartsWith:   .asciz "String_startsWith_1(s1,11,""hat."") = "
-    szStartsWith2:  .asciz "String_startsWith_2(s1,""Cat"") = "
-    szEndswith:     .asciz "String_endsWith(s1,""in the hat."") = "
-    szindexof1:     .asciz "String_indexOf_1(s2,'g') = "
-    szindexof2:     .asciz "String_indexOf_2(s2,'g',9) = "
-    szindexof3:     .asciz "String_indexOf_3(s2,""eggs"") = "
-    szlastindexof1: .asciz "String_lastIndexOf_1(s2,'g') = "
-    szlastindexof2: .asciz "String_lastIndexOf_2(s2,'g',6) = "
-    szlastindexof3: .asciz "String_lastIndexOf_3(s2,""egg"") = "
-    szreplace:      .asciz "String_replace(s1,'a','o') = "
-    sztolower:      .asciz "String_toLowerCase(s1) = "
-    sztoupper:      .asciz "String_toUpperCase(s1) = "
-    szConcat:       .asciz "String_concat(s1, s2) = "
+    szLen1:         .asciz "s1.length() = " 						//Length of str1
+    szLen2:         .asciz "s2.length() = " 						//Length of str2
+    szLen3:         .asciz "s3.length() = " 						//Length of str3
+    szEquals:       .asciz "String_equals(s1,s3) = "				//Equals string to output
+    szEquals2:      .asciz "String_equals(s1,s1) = "				//Equals 2 string to output
+    szEqIgnore:     .asciz "String_equalsIgnoreCase(s1,s3) = "		//Equals ignore case string to output
+    szEqIgnore2:    .asciz "String_equalsIgnoreCase(s1,s2) = "		//Equals ignore case 2 string to output
+    szCpy:          .asciz "s4 = String_copy(s1)"					//Copy string to output
+    szSubStr1:      .asciz "String_substring_1(s3,4,14) = "			//Substr1 string to output
+    szSubStr2:      .asciz "String_substring_2(s3,7) = "			//Substr2 string to output
+    szCharAt:       .asciz "String_charAt(s2,4) = "					//Char at string to output
+    szStartsWith:   .asciz "String_startsWith_1(s1,11,""hat."") = "	//Starts with string to output
+    szStartsWith2:  .asciz "String_startsWith_2(s1,""Cat"") = "		//Starts with 2 string to output
+    szEndswith:     .asciz "String_endsWith(s1,""in the hat."") = "	//Ends with string to output
+    szindexof1:     .asciz "String_indexOf_1(s2,'g') = "			//Index of 1 string to output
+    szindexof2:     .asciz "String_indexOf_2(s2,'g',9) = "			//Index of 2 string to output
+    szindexof3:     .asciz "String_indexOf_3(s2,""eggs"") = "		//Index of 3 string to output
+    szlastindexof1: .asciz "String_lastIndexOf_1(s2,'g') = "		//Last index of 1 string to output
+    szlastindexof2: .asciz "String_lastIndexOf_2(s2,'g',6) = "		//Last index of 2 string to output
+    szlastindexof3: .asciz "String_lastIndexOf_3(s2,""egg"") = "	//Last index of 3 string to output
+    szreplace:      .asciz "String_replace(s1,'a','o') = "			//Replace string to output
+    sztolower:      .asciz "String_toLowerCase(s1) = "				//Tolower string to output
+    sztoupper:      .asciz "String_toUpperCase(s1) = "				//Toupper string to output
+    szConcat:       .asciz "String_concat(s1, s2) = "				//Concat string to output
 
     chLF:   .byte 0xa   //Hex for line feed
-    chBuff: .byte 0x0
-    chg:    .byte 0x67
-    cha:    .byte 0x61
-    cho:    .byte 0x6f
+    chBuff: .byte 0x0	//Char buffer
+    chg:    .byte 0x67	//g
+    cha:    .byte 0x61	//a
+    cho:    .byte 0x6f	//o
 	.text
 _start:
 
@@ -84,19 +84,19 @@ _start:
     bl putch            //Branch and link to putch
 
     ldr x0,=szPrompt    //Load x0 with the prompt
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS1
     mov x1, BUFFER
     bl getstring
 
     ldr x0,=szPrompt
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS2
     mov x1, BUFFER
     bl getstring
 
     ldr x0,=szPrompt
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS3
     mov x1, BUFFER
     bl getstring
@@ -107,9 +107,9 @@ _start:
     ldr x1, =szBuffer
     bl int64asc
     ldr x0,=szLen1
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szBuffer
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
 
@@ -118,9 +118,9 @@ _start:
     ldr x1, =szBuffer
     bl int64asc
     ldr x0,=szLen2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szBuffer
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
 
@@ -129,9 +129,9 @@ _start:
     ldr x1, =szBuffer
     bl int64asc
     ldr x0,=szLen3
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szBuffer
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
 
@@ -143,15 +143,15 @@ _start:
     cmp x1,0x0
     b.eq main_eq_0
     ldr x0,=szEquals
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szTrue
-    bl putstring
+    bl putstring		//Branch and link to putstring
     b main_eq_next
     main_eq_0:
     ldr x0,=szEquals
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szFalse
-    bl putstring
+    bl putstring		//Branch and link to putstring
     main_eq_next:
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
@@ -166,15 +166,15 @@ _start:
     cmp x1,0x0
     b.eq main_eq_0_2
     ldr x0,=szEquals2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szTrue
-    bl putstring
+    bl putstring		//Branch and link to putstring
     b main_eq_next_2
     main_eq_0_2:
     ldr x0,=szEquals2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szFalse
-    bl putstring
+    bl putstring		//Branch and link to putstring
     main_eq_next_2:
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
@@ -189,15 +189,15 @@ _start:
     cmp x1,0x0
     b.eq main_eqig_0
     ldr x0,=szEqIgnore
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szTrue
-    bl putstring
+    bl putstring		//Branch and link to putstring
     b main_eqig_next
     main_eqig_0:
     ldr x0,=szEqIgnore
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szFalse
-    bl putstring
+    bl putstring		//Branch and link to putstring
     main_eqig_next:
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
@@ -212,15 +212,15 @@ _start:
     cmp x1,0x0
     b.eq main_eqig_0_2
     ldr x0,=szEqIgnore2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szTrue
-    bl putstring
+    bl putstring		//Branch and link to putstring
     b main_eqig_next_2
     main_eqig_0_2:
     ldr x0,=szEqIgnore2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szFalse
-    bl putstring
+    bl putstring		//Branch and link to putstring
     main_eqig_next_2:
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
@@ -229,22 +229,22 @@ _start:
 
     //Test 6
     ldr x0,=szCpy
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
     ldr x0,=szS1eq
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS1
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
     ldr x0,=szS4eq
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS1
     bl String_copy
     ldr x1,=dbPtr
     str x0,[x1]
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
     ldr x0,=dbPtr
@@ -258,14 +258,14 @@ _start:
 
     //Test 7
     ldr x0,=szSubStr1
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS3
     mov x1, #4
     mov x2, #14
     bl String_substring_1
     ldr x1,=dbPtr
     str x0,[x1]
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
     ldr x0,=dbPtr
@@ -279,13 +279,13 @@ _start:
 
     //Test 8
     ldr x0,=szSubStr2
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=szS3
     mov x1, #7
     bl String_substring_2
     ldr x1,=dbPtr
     str x0,[x1]
-    bl putstring
+    bl putstring		//Branch and link to putstring
     ldr x0,=chLF        //Load x0 with the address of chLF
     bl putch            //Branch and link to putch
     ldr x0,=dbPtr
